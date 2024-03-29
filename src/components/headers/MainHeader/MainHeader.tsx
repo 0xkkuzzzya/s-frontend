efea4f0258d4c2e274666dbc6cdde1aceef5d469
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-03-30 00:03:49 +0500

    feat: adding connect keplr

--- src/components/headers/MainHeader/MainHeader.tsx
+++ src/components/headers/MainHeader/MainHeader.tsx
@@ -1,16 +1,27 @@
 import styled from "styled-components";
+import QubeLogo from '../../../assets/svg/QubeLogo.webp'
+import { ConnectButton } from "../../Buttons/ConnectButton/ConnectButton";
 
 const Header = styled.div`
     width: 100%;
-    height: 100px;
+    height: 80px;
     background: #202020;
+    display: flex;
+    align-items: center;
+`
+
+const Logo = styled.img`
+    width: 55px;
+    height: 55px;
+    margin-left: 60px;
 `
 
 
 export const MainHeader = () => {
     return(
         <Header>
-            
+            <Logo src={QubeLogo}/>
+            <ConnectButton/> 
         </Header>
     )
 }
