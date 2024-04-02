6d12dcb5f89c259acac09a3374c7621441403b61
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-04-02 14:15:53 +0500

    feat: added soon page

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import { SwapPage } from "./Pages/Swap/SwapPage";
 import { MainHeader } from "./Header/MainHeader/MainHeader";
 import { useToggleTheme } from "../hooks/useToggleTheme";
+import { Pools } from "./Pages/Pools/Pools";
+import { Orderbook } from "./Pages/Orderbook/Orderbook";
+import { Futures } from "./Pages/Futures/Futures";
+import { Deposit } from "./Pages/Deposit/Deposit";
 
 const Container = styled.div <{backgroundColor: string}>`
     display: flex;
@@ -26,11 +30,11 @@ export const MainIndex = () => {
             <MainHeader />
             <PageContainer>
                 <Routes>
-                    <Route path="/orderbook" element={<></>} />
-                    <Route path="/pools" element={<></>} />
+                    <Route path="/orderbook" element={<Orderbook/>} />
+                    <Route path="/pools" element={<Pools/>} />
                     <Route path="/" element={<SwapPage />} />
-                    <Route path="/futures" element={<></>} />
-                    <Route path="/deposit" element={<></>} />
+                    <Route path="/futures" element={<Futures/>} />
+                    <Route path="/deposit" element={<Deposit/>} />
                 </Routes>
             </PageContainer>
         </Container>
