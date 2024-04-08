84196401977a7a1df848b3a67db7bd12fe5890fd
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-04-08 14:28:13 +0500

    fix: fixed confirm button

--- src/components/Modal/PageModal/ModalTransaction/SwapPageTransaction/SwapModalTransaction.tsx
+++ src/components/Modal/PageModal/ModalTransaction/SwapPageTransaction/SwapModalTransaction.tsx
@@ -44,10 +44,10 @@ const CloseButton = styled.button <{ TextColor: string }>`
 `
 
 const OpenButton = styled.button`
-    width: 90%;
+    width: 100%;
     height: 50px;
-    font-size: 16px;
-    font-weight: 700;
+    font-size: 18px;
+    font-weight: 600;
     background: linear-gradient(to left, #3B9CFC, #6CBBFF);
     border: none;
     margin: 0 auto;
